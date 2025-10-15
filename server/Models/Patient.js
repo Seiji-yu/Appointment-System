348e3e184903cb0c8a7f05bfeaa08af96ca88307
tati4na <nodaalexis21@gmail.com>
2025-10-15 20:34:57 +0800

    Update Patient.js

--- server/Models/Patient.js
+++ server/Models/Patient.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const PatientSchema = new mongoose.Schema({
     name: String,
     email: String,
+    firstName: String,
+    lastName: String,
+    birthday: Date,
+    age: Number,
+    gender: String,
+    contact: String,
+    address: String,
+    medicalHistory: String,
     password: String,
     role: { type: String, enum: ['Patient'], required: true }
 });
