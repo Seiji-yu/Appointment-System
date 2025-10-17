3fb7a347118c33a02c2717fff58ef24d1aa42d50
Sean <seanpaustine@gmail.com>
2025-10-17 21:09:04 +0800

    Fixed merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-<<<<<<< HEAD
 import Signup from './Auth/Signup'
 import Login from './Auth/Login'
 import Dashboard from './Pages/DoctorSide/Ddashboard'
@@ -14,20 +13,12 @@ import PDashboard from './Pages/PatientSide/PDashboard'
 import BookApp from './Pages/PatientSide/BookApp'
 import DoctorLists from './Pages/PatientSide/DoctorLists'
 import AppHistory from './Pages/PatientSide/AppHistory'
-=======
-import Signup from './Auth/Signup.jsx'
-import Login from './Auth/Login.jsx'
-import Dashboard from './Pages/Ddashboard'
-import PatientProfileForm from './Pages/PatientProfileForm'
-import PatientDashboard from './Pages/PDashboard'
->>>>>>> 78f26d55f55ef3828c80996bc0bcec69889ffe11
 
 function App() {
 
   return (
     <BrowserRouter>
       {/* Routes define which page is shown */}
-<<<<<<< HEAD
       <Routes>
         <Route path='/' element={<Signup />} />
         <Route path='/register' element={<Signup />} />
@@ -43,16 +34,6 @@ function App() {
         <Route path='/BookApp' element={<BookApp />} />
         <Route path='/AppHistory' element={<AppHistory />} />
       </Routes>
-=======
-    <Routes>
-      <Route path='/' element={<Signup/>}> </Route>
-      <Route path='/register' element={<Signup/>}> </Route>
-      <Route path='/login' element={<Login/>}> </Route>
-      <Route path='/dashboard' element={<Dashboard/>}> </Route>
-      <Route path='/patient/profile' element={<PatientProfileForm/>}> </Route>
-      <Route path='/patient/dashboard' element={<PatientDashboard/>}> </Route>
-    </Routes>
->>>>>>> 78f26d55f55ef3828c80996bc0bcec69889ffe11
     </BrowserRouter>
   )
 }
